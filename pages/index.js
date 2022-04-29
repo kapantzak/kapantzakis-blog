3bff2af6712513efc7ee98af1416c889e1de6d45
John Kapantzakis <kapantzak@gmail.com>
2022-04-29 10:46:37 +0300

    Add mongodb

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
-import Image from "next/image";
+import clientPromise from "../lib/mongodb";
 
-export default function Home() {
+export default function Home({ isConnected }) {
   return (
     <div>
       <Head>
@@ -14,7 +14,32 @@ export default function Home() {
         <h1>My Portfolio / Blog</h1>
 
         <p>I&apos;m a web developer</p>
+
+        {isConnected && <p>DB connected</p>}
       </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  try {
+    await clientPromise;
+    // `await clientPromise` will use the default database passed in the MONGODB_URI
+    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the folloing code:
+    //
+    // `const client = await clientPromise`
+    // `const db = client.db("myDatabase")`
+    //
+    // Then you can execute queries against your database like so:
+    // db.find({}) or any of the MongoDB Node Driver commands
+
+    return {
+      props: { isConnected: true },
+    };
+  } catch (e) {
+    console.error(e);
+    return {
+      props: { isConnected: false },
+    };
+  }
 }
