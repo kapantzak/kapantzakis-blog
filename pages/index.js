755778ecb5f7bfdcd7bb4e0e66423cd42b413c64
John Kapantzakis <kapantzak@gmail.com>
2022-05-06 17:52:42 +0300

    Remove mongodb related stuff

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
-import clientPromise from "../lib/mongodb";
 
-export default function Home({ isConnected }) {
+export default function Home() {
   return (
     <div>
       <Head>
@@ -14,32 +13,7 @@ export default function Home({ isConnected }) {
         <h1>My Portfolio / Blog</h1>
 
         <p>I&apos;m a web developer</p>
-
-        {isConnected && <p>DB connected</p>}
       </main>
     </div>
   );
-}
-
-export async function getStaticProps(context) {
-  try {
-    await clientPromise;
-    // `await clientPromise` will use the default database passed in the MONGODB_URI
-    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the folloing code:
-    //
-    // `const client = await clientPromise`
-    // `const db = client.db("myDatabase")`
-    //
-    // Then you can execute queries against your database like so:
-    // db.find({}) or any of the MongoDB Node Driver commands
-
-    return {
-      props: { isConnected: true },
-    };
-  } catch (e) {
-    console.error(e);
-    return {
-      props: { isConnected: false },
-    };
-  }
 }
