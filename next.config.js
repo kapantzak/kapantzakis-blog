26e3c94d180658b8639c4f8a6a86f7744f523f29
John Kapantzakis <kapantzak@gmail.com>
2022-05-06 16:38:40 +0300

    Remove webpack section from next.config.js

--- next.config.js
+++ next.config.js
@@ -6,32 +6,6 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-  webpack: config => {
-    config.resolve.fallback = {
-      fs: false,
-      crypto: false,
-      stream: false,
-      process: false,
-      http: false,
-      net: false,
-      zlib: false,
-      util: false,
-      events: false,
-      dns: false,
-      tls: false,
-      path: false,
-      querystring: false,
-      os: false,
-      kerberos: false,
-      snappy: false,
-      aws4: false,
-      "bson-ext": false,
-      "mongodb-client-encryption": false,
-      "snappy/package.json": false,
-    };
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;
