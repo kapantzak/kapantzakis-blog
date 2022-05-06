5cb2db704d080c81ed4500ec1c8fd3d505f210e6
John Kapantzakis <kapantzak@gmail.com>
2022-05-06 16:42:39 +0300

    Use mongoose to connect to database

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import clientPromise from "../lib/mongodb";
+import dbConnect from "../lib/dbConnect";
 
 export default function Home({ isConnected }) {
   return (
@@ -23,15 +23,7 @@ export default function Home({ isConnected }) {
 
 export async function getStaticProps(context) {
   try {
-    await clientPromise;
-    // `await clientPromise` will use the default database passed in the MONGODB_URI
-    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the folloing code:
-    //
-    // `const client = await clientPromise`
-    // `const db = client.db("myDatabase")`
-    //
-    // Then you can execute queries against your database like so:
-    // db.find({}) or any of the MongoDB Node Driver commands
+    await dbConnect();
 
     return {
       props: { isConnected: true },
